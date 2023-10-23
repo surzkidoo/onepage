00138a07cc65027260376bde7e93893cac867394
surzkidoo <abubakarsurzkidoo@gmail.com>
2023-10-23 13:03:13 +0100

    menu-fix

---
+++ index.js
@@ -0,0 +1,15 @@
+var sidebar = document.querySelector('.sidebar-container')
+        var overlay = document.querySelector('.mobile-overlay')
+        var closemenu = document.querySelector('.close-menu')
+        var menu = document.querySelector('.menu-toggler')
+
+        menu.addEventListener('click',()=>{
+            sidebar.style.display= 'flex'
+            overlay.style.display= 'flex'
+
+            closemenu.addEventListener('click',()=>{
+                sidebar.style.display= 'none'
+            overlay.style.display= 'none'
+
+            })
+        })
